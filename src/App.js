10288b98a81ab1ff22baf081d1e517712c5d701b
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2020-10-28 13:29:12 -0300

    initial routes added to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ProductsStorage } from './ProductsContext'
+import Header from './Components/Header'
+import Sneakers from './Components/Sneakers'
+import Checkout from './Components/Checkout/Checkout'
 
 const App = () => {
   return (
     <div>
       <ProductsStorage>
-
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Sneakers />} />
+            <Route path='/checkout/:id' element={<Checkout />} />
+          </Routes>
+        </BrowserRouter>
       </ProductsStorage>
     </div>
   )
